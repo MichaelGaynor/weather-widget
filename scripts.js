1853c28240a33fa7616ba4fd7f3fb10510866f4e
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-05-23 14:27:52 -0400

    functional api call

--- scripts.js
+++ scripts.js
@@ -1,38 +1,49 @@
 $(document).ready(()=>{
 
+    const weatherApi ='http://api.openweathermap.org/data/2.5/weather';
 
     $('#weather-form').submit(()=>{
         event.preventDefault();
         var zipCode = $('#zip-code').val();
-        console.log(zipCode);
+        var weatherData = weatherApi + '?zip=' + zipCode + ',us&units=imperial&appid=' +apiKey;
+        $.getJSON(weatherData, (weatherData)=>{
+            var currTemp = weatherData.main.temp;
+            var temps = weatherData.main;
+
+            var name = weatherData.name;
+            var icon = weatherData.weather[0].icon + '.png';
+            var newHtml = '<img src="http://openweathermap.org/img/w/' +icon+ '">';
+            newHtml +='<div>the temp in ' +name+ ' is currently ' +currTemp+ '&deg;</div>';
+            $('#temp-info').html(newHtml);
+            currentPercent = 0;
+            animateCircle(0,currTemp);
+        })
     })
 
 
     var canvas = $('#weather-canvas');
     var context = canvas[0].getContext('2d');
-
     var assumedTemperature = 65;
-
     var currentPercent = 0;
 
-    function animateCircle(currentArc){
-        context.fillStyle = '#ccc';
+    function animateCircle(currentArc, currentTemp){
+        context.fillStyle = '#395E66';
         context.beginPath();
         context.arc(155,175,70,Math.PI*0,Math.PI*2);
         context.closePath();
         context.fill();
 
         context.lineWidth = 20;
-        context.strokeStyle = '#395E66';
+        context.strokeStyle = '#2BC016';
         context.beginPath();
         context.arc(155,175,80,Math.PI*1.5,(Math.PI*2*currentArc)+(Math.PI*1.5));
         context.stroke();
         currentPercent++
-        if(currentPercent < assumedTemperature){
+        if(currentPercent < currentTemp){
             requestAnimationFrame(()=>{
-                animateCircle(currentPercent/100);
+                animateCircle(currentPercent/100,currentTemp);
             })
         }
     }
-    animateCircle();
+    // animateCircle();
 })
